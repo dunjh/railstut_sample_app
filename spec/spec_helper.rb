ae327629b9110188450f3a89637a1591e60fbfbf
Dun Jinhui <dunjinh@gmail.com>
2013-11-20 22:26:45 +0800

    Add a full_title helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,18 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -41,4 +49,10 @@ RSpec.configure do |config|
   config.order = "random"
 
   config.include Capybara::DSL
+end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
