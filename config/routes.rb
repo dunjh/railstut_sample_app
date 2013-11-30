a2c9b9f66a13742cf1513560ddab32899c999dde
Dun Jinhui <dunjinh@gmail.com>
2013-11-30 23:07:10 +0800

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ RailstutSampleApp::Application.routes.draw do
   match '/signup', to: 'users#new', via: 'get'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   match '/signin',  to: 'sessions#new',     via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
